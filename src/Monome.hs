194973401531c17888b75e1cc93b3a4bf6faf011
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-10-18 23:18:46 -0500

    interpret a serialosc message

--- src/Monome.hs
+++ src/Monome.hs
@@ -7,22 +7,50 @@
 
 module Monome where
 
-import Control.Concurrent (threadDelay)
+import qualified Control.Concurrent as CC
+import Control.Concurrent.MVar
 import Control.Monad (forever)
+import qualified Data.ByteString as BS
 import Network.Socket
-import Network.Socket.ByteString as SB
+import qualified Network.Socket.ByteString as NSB
 import Vivid.OSC
 
 import Util
 
 
+data Device = Device { deviceName :: BS.ByteString
+                     , deviceType :: BS.ByteString
+                     , devicePort :: Int } deriving (Show, Eq, Ord)
+
+readDevice :: OSC -> Device
+readDevice ( OSC "/serialosc/device" [ OSC_S name
+                                     , OSC_S monomeType
+                                     , OSC_I port ] )
+  = Device { deviceName = name
+           , deviceType = monomeType
+           , devicePort = fromIntegral port }
+
 requestMonomeInfo = do
   s <- sendsTo "127.0.0.1" 12002
-  SB.send s $ encodeOSC $ OSC "/serialosc/list" [
+  NSB.send s $ encodeOSC $ OSC "/serialosc/list" [
     OSC_S "127.0.0.1"
     , OSC_I 11111
     ]
 
-monomeMailbox = do
+mailbox :: IO [OSC]
+mailbox = do
   s <- receivesAt "127.0.0.1" 11111
-  forever $ decodeOSC <$> SB.recv s 4096 >>= putStrLn . show
+  acc <- newMVar []
+  let loop = do cmd <- getChar
+                case cmd of 'q' -> readMVar acc >>= return
+                            _ -> loop
+      printAndShow :: OSC -> IO ()
+      printAndShow osc = do accNow <- takeMVar acc
+                            putMVar acc $ osc : accNow
+                            putStrLn . show $ osc
+      printAndShowEitherOsc :: Either String OSC -> IO ()
+      printAndShowEitherOsc (Left s) = putStrLn . show $ s
+      printAndShowEitherOsc (Right osc) = printAndShow osc
+  mailbox <- CC.forkIO $
+    forever $ decodeOSC <$> NSB.recv s 4096 >>= printAndShowEitherOsc
+  loop
---
+++ src/meh/BreakLoop.hs
@@ -0,0 +1,4 @@
+module Break where
+
+-- http://www.haskellforall.com/2012/07/breaking-from-loop.html
+-- I can't find EitherT in recent versions of Stackage
