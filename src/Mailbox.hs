9d7ffda4c3559ec0775afd39a4b128c87a26e274
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-10-20 20:42:04 -0500

    for pitch, take x, y into account for amp, take pressed|unpressed into accuont keep a voice around, rather than making a new one for each press

--- src/Mailbox.hs
+++ src/Mailbox.hs
@@ -13,29 +13,36 @@ import Util.Network
 import Device
 
 
-theSound = sd (0 ::I "note") $ do
-   wobble <- sinOsc (freq_ 5) ? KR ~* 10 ~+ 10
-   s <- 0.1 ~* sinOsc (freq_ $ midiCPS (V::V "note") ~+ wobble)
-   out 0 [s,s]
+type BoopParams = '["freq","amp"]
 
-playKey :: OSC -> IO ()
-playKey osc@(OSC "/monome/grid/key" _) =
-  let (Press x y s) = readPress osc
-  in do s0 <- synth theSound (36 ::I "note")
-        wait 1
-        free s0
-playKey osc = putStrLn . show $ osc
+boop :: SynthDef BoopParams
+boop = sd ( 0 :: I "freq"
+          , 0 :: I "amp"
+          ) $ do
+   s1 <- (V::V "amp") ~* sinOsc (freq_ (V::V "freq"))
+   out 0 [s1, s1]
+
+playKey :: Synth BoopParams -> OSC -> IO ()
+playKey sy osc@(OSC "/monome/grid/key" _) = do
+  putStrLn $ show osc
+  let (Press x y p) = readPress osc
+  set sy $ (toI $ 0.02 * fi (pressureToInt p) :: I "amp")
+  set sy $ (toI $ 100 + x + 10*y :: I "freq")
+playKey _ osc = putStrLn . show $ osc
 
 mailboxSynths :: IO [OSC]
 mailboxSynths = do
   s <- receivesAt "127.0.0.1" 11111
   acc <- newMVar []
+--  spots = [(a,b) | a <- [1..16], b <- [1..16]]
+--  voices <- zip spots <$> mapM (synth boop) (repeat $ 0 :: I "amp")
+  voice <- synth boop ()
   let loop :: IO [OSC]
       loop = do cmd <- getChar
                 case cmd of 'q' -> close s >> readMVar acc >>= return
                             _   -> loop
   mailbox <- forkIO $ forever $
-    decodeOSC <$> recv s 4096 >>= either (putStrLn . show) playKey
+    decodeOSC <$> recv s 4096 >>= either (putStrLn . show) (playKey voice)
   loop
 
 -- ^ Tries to read as OSC, then prints (as OSC or otherwise).
