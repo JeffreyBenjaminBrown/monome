f390c7875425897133fe4376a6c8aab806245e5a
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-10-27 09:55:31 -0500

    nothing

--- src/Test.hs
+++ src/Test.hs
@@ -11,16 +11,16 @@ tests = runTestTT $ TestList [
     ]
 
 testBelongsHere = TestCase $ do
-  let emptyHandler = const $ const $ return ()
+  let emptyHandler = \_ _ _ -> return ()
       w1 = Window "w1" (\(x,y) -> x > y) emptyHandler
       w2 = Window "w2" (\(x,y) -> x > 4) emptyHandler
       w3 = Window "w3" (\(x,y) -> True)  emptyHandler
       ws = [w1,w2,w3]
-  assertBool "?" $ not $ belongsHere ws w3 ((1,0),LedOn)
-    -- caught by w1 before reaching w3
-  assertBool "?" $ not $ belongsHere ws w3 ((5,6),LedOn)
-    -- caught by w2 before reaching w3
-  assertBool "?" $ belongsHere ws w3 ((0,0),LedOn)
-    -- should reach w3, which contains it
-  assertBool "?" $ not $ belongsHere ws w2 ((1,2),LedOn)
-    -- should reach w2, but w2 does not contain it
+  assertBool "caught by w1 before reaching w3" $
+    not $ belongsHere ws w3 ((1,0),LedOn)
+  assertBool "caught by w2 before reaching w3" $
+    not $ belongsHere ws w3 ((5,6),LedOn)
+  assertBool "should reach w3, which contains it" $
+    belongsHere ws w3 ((0,0),LedOn)
+  assertBool "should reach w2, but w2 does not contain it" $
+    not $ belongsHere ws w2 ((1,2),LedOn)
