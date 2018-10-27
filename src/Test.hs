5a5cadd90287e0423bdfadf5c43f3a2004a87145
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-10-26 23:58:12 -0500

    for determining whether a cell is obscured by a higher window   +belongsHere :: [Window] -> Window -> LedFilter   +playIfHere :: Socket -> [Window] -> Window -> LedRelay

--- src/Test.hs
+++ src/Test.hs
@@ -2,9 +2,25 @@ module Test (tests) where
 
 import Test.HUnit
 
+import Types.App
+import Types.Button
+
+
 tests = runTestTT $ TestList [
-    TestLabel "testTest" testTest
+    TestLabel "testBelongsHere" testBelongsHere
     ]
 
-testTest = TestCase $ do
-  assertBool "hunit works" $ 4 > 3
+testBelongsHere = TestCase $ do
+  let emptyHandler = const $ const $ return ()
+      w1 = Window "w1" (\(x,y) -> x > y) emptyHandler
+      w2 = Window "w2" (\(x,y) -> x > 4) emptyHandler
+      w3 = Window "w3" (\(x,y) -> True)  emptyHandler
+      ws = [w1,w2,w3]
+  assertBool "?" $ not $ belongsHere ws w3 ((1,0),LedOn)
+    -- caught by w1 before reaching w3
+  assertBool "?" $ not $ belongsHere ws w3 ((5,6),LedOn)
+    -- caught by w2 before reaching w3
+  assertBool "?" $ belongsHere ws w3 ((0,0),LedOn)
+    -- should reach w3, which contains it
+  assertBool "?" $ not $ belongsHere ws w2 ((1,2),LedOn)
+    -- should reach w2, but w2 does not contain it
--- src/Types/App.hs
+++ src/Types/App.hs
@@ -12,6 +12,26 @@ import Types.Button
 import Util.Network
 
 
+-- | `LedRelay` is for preventing one Window from writing to
+-- the `Led`s of another `Window`.
+type LedRelay = ((X,Y), Led) -> IO ()
+type LedFilter = ((X,Y), Led) -> Bool
+
+belongsHere :: [Window] -> Window -> LedFilter
+belongsHere ws w = f where
+  above = takeWhile (/= w) ws -- the windows above `w`
+  obscured :: (X,Y) -> Bool
+  obscured xy = or $ map ($ xy) $ map windowContains above
+  f :: ((X,Y), Led) -> Bool
+  f (xy,_) = not (obscured xy) && windowContains w xy
+
+playIfHere :: Socket -> [Window] -> Window -> LedRelay
+playIfHere toMonome ws w = f where
+  f :: ((X,Y),Led) -> IO ()
+  f msg = if belongsHere ws w msg
+    then (send toMonome $ ledOsc "/monome" msg) >> return ()
+    else return ()
+
 data State = State {
     inbox :: Socket
   , toMonome :: Socket
@@ -26,7 +46,11 @@ data State = State {
 data Window = Window {
     windowLabel :: String
   , windowContains :: (X,Y) -> Bool
-  , windowHandler :: MVar State -> ((X,Y), Switch) -> IO () }
+  , windowHandler :: MVar State
+--    -> LedRelay -- ^ Windows speaks to device through this
+    -> ((X,Y), Switch) -- ^ the incoming button press|release
+    -> IO ()
+  }
 
 instance Eq Window where
   (==) a b = windowLabel a == windowLabel b
