59b395d9ded21cb727837e2ff4ac8ba13a6fcd0b
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-02-15 21:11:24 -0500

    delete drawPitchClass   it's obsolete now

--- Monome/Window/Common.hs
+++ Monome/Window/Common.hs
@@ -12,18 +12,6 @@ import Monome.Types.Button
 import Monome.Util
 
 
--- TODO : delete once unneeded
-drawPitchClass :: LedRelay
-               -> (X,Y)      -- ^ a shift
-               -> Led
-               -> PitchClass -- ^ what to light up
-               -> IO ()
-drawPitchClass toKeyboardWindow xyShift led pitchClass = do
-  let xys = enharmonicToXYs $
-            addPair (et31ToLowXY pitchClass) xyShift
-      f = toKeyboardWindow . (,led)
-  mapM_ f xys
-
 -- TODO (#speed) Instead, keep a map from xy to pitchclass
 ledBecause_toPitchClass ::
      LitPitches
